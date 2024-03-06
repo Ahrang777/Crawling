a120f3d432d392e8f108599be80d1b1e0843cce5
박정인 <wjddlsee@gmail.com>
2024-03-06 15:12:18 +0900

    chore: 프로젝트 생성

---
+++ src/test/java/org/example/crawling/CrawlingApplicationTests.java
@@ -0,0 +1,13 @@
+package org.example.crawling;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class CrawlingApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
